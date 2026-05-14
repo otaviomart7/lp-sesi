7d494490390158a2b02d44a67c5a9fba047eea78
Otávio Martineschen <otaviomart7tds@gmail.com>
2026-05-14 12:30:38 -0300

    asasa

--- src/script.js
+++ src/script.js
@@ -1,13 +1,15 @@
 window.addEventListener('scroll', () => {
     const nav = document.querySelector('nav')
+    const footer = document.querySelector('footer')
 
     if(window.scrollY > 0){
         nav.classList.add('sc')
+        footer.classList.add('ft')
     }else{
         nav.classList.remove('sc')
+        footer.classList.remove('ft')
     }
 })
-
 
 function navTo(id){
     document.getElementById(id).scrollIntoView({
