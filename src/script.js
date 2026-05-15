26475ef27d19f2783e0d7d245852fc5a39248349
Otávio Martineschen <otaviomart7tds@gmail.com>
2026-05-15 09:30:40 -0300

    asas

--- src/script.js
+++ src/script.js
@@ -17,3 +17,23 @@ function navTo(id){
         block: 'start'
     })
 }
+
+const cards = document.querySelectorAll('#s2, .card')
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entries) => {
+        if(entries.isIntersecting){
+            cards.forEach((card, index) => {
+                setTimeout{() => {
+                    card.classList.add('visible');
+                }, index * 500);
+            });
+        }else{
+            cards.forEach((card) => {
+                card.classList.remove('visible')
+            })
+        }
+    });
+}, {threshold: 0.2})
+
+observer.observe(document.querySelector('#s2'))
